b6db936be4c741547f62736f88c2103e282d5f93
Ankur Sinha <ankur218@yahoo.com>
2017-09-15 13:14:23 +0200

    Fixed insertion of comments sentence wise to db

--- thesis-data-analysis/lucene-rs.py
+++ thesis-data-analysis/lucene-rs.py
@@ -1,12 +1,17 @@
+# -*- coding: utf-8 -*-
 import pymysql
 import pymysql.cursors
 import random
 from bs4 import BeautifulSoup
 import nltk
 from nltk import tokenize
+from time import time
 
-conn = pymysql.connect(host='localhost', user='root', password='password', db='master_thesis', autocommit=True)
+start_time = time()
+
+conn = pymysql.connect(host='localhost', user='root', password='password', db='master_thesis', autocommit=True, use_unicode=True, charset="utf8")
 cursor = conn.cursor()
+cursor2 = conn.cursor()
 cursor.execute("SELECT * FROM lucene_counter WHERE count > 5 AND count <= 30")
 
 lucene_rs_list = list()
@@ -15,22 +20,29 @@ for row in cursor:
     lucene_rs_list.append(row[1])
 
 random.shuffle(lucene_rs_list)
-final_list = lucene_rs_list[:1]
+final_list = lucene_rs_list[:50]
 
 for i in range(len(final_list)):
+    print("PRINTING ISSUE NUMBER: ", i+1)
     current_title = final_list[i]
     query = "SELECT title, comment FROM lucene_try WHERE title = %s"
     cursor.execute(query, final_list[i])
     for row in cursor:
         root_comment = BeautifulSoup(row[1], "lxml").text
         print("Root Title: ", current_title)
-        sub_comment = tokenize.sent_tokenize(root_comment)
         print("Root Comment: ", root_comment)
-        for j in range(len(sub_comment)):
-            print("Sub Comment: ", sub_comment[j])
-            #cursor.execute("""INSERT INTO lucene_rs (title, root_comment, comment) VALUES ("%s", "%s", "%s")""" % (current_title, root_comment, sub_comment[j]))
-            #conn.commit()
+        cleancomment = tokenize.sent_tokenize(root_comment)
+        for j in range(len(cleancomment)):
+            print("Sub Comment: ", cleancomment[j])
+            try:
+                cursor2.execute("""INSERT INTO lucene_rs (title, root_comment, comment) VALUES ("%s", "%s", "%s")""" % (current_title, root_comment, cleancomment[j]))
+            except:
+                print()
         print("\n")
-    print("\n")
 
 conn.close()
+
+end_time = time()
+time_taken = end_time - start_time
+
+print("Total time taken in seconds: ", time_taken)
