bec3b00f5668c5065c51b597b00359672c24bdf2
Ankur Sinha <ankur218@yahoo.com>
2017-09-17 11:04:42 +0200

    Fixed code to escape strings

--- thesis-data-fetching/lucene-data/lucene-scraper.py
+++ thesis-data-fetching/lucene-data/lucene-scraper.py
@@ -4,8 +4,11 @@ import xml.etree.ElementTree as ET
 from time import time
 import pymysql
 import pymysql.cursors
+from bs4 import BeautifulSoup
+import sys
+import re
 
-db = pymysql.connect(host='localhost', user='root', password='password', db='master_thesis')
+db = pymysql.connect(host='localhost', user='root', password='password', db='master_thesis', autocommit=True, use_unicode=True, charset="utf8")
 cursor = db.cursor()
 
 start_time = time()
@@ -21,11 +24,19 @@ for i in range(8):
         for item in channel.iter('item'):
             title = item.find('title').text
             description = item.find('description').text
+            try:
+                description = re.escape(description)
+            except TypeError:
+                description = description
             assignee = item.find('assignee').text
             reporter = item.find('reporter').text
             for comment_author in item.iter('comment'):
                 author = comment_author.get('author')
                 comment = comment_author.text
+                try:
+                    comment = re.escape(comment)
+                except TypeError:
+                    comment = comment
                 print("Parsing scraped data from ", curxml)
                 print("Title: ", title)
                 print("Description: ", description)
@@ -33,7 +44,7 @@ for i in range(8):
                 print("Reporter: ", reporter)
                 print("Comment: ", comment)
                 print("Author: ", author)
-
+                #tagged = 'NULL'
                 refstr = "ViewProfile.jspa?name="
                 if refstr in comment:
                     x = comment.split(refstr, 1)
@@ -41,11 +52,11 @@ for i in range(8):
                     tagged = x[0]
                     print("Tagged: ", tagged)
                 else:
+                    tagged = 'NULL'
                     print("Tagged: None")
 
                 try:
                     cursor.execute("""INSERT INTO lucene_try (title, description, comment, author, reporter, assignee, tagged) VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s")""" % (title, description, comment, author, reporter, assignee, tagged))
-                    db.commit()
                 except:
                     db.rollback()
 
