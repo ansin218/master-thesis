d00b3d3cce886c8caae6c4c0e6c4f827e80b4024
Ankur Sinha <ankur218@yahoo.com>
2017-10-01 08:21:34 +0200

    Added thunderbird random sampling script

--- thesis-data-analysis/thunderbird-rs.py
+++ thesis-data-analysis/thunderbird-rs.py
@@ -13,24 +13,24 @@ start_time = time()
 conn = pymysql.connect(host='localhost', user='root', password='password', db='master_thesis', autocommit=True, use_unicode=True, charset="utf8")
 cursor = conn.cursor()
 cursor2 = conn.cursor()
-cursor.execute("SELECT * FROM lucene_counter WHERE count > 5 AND count <= 30")
+cursor.execute("SELECT * FROM thunderbird_counter WHERE count > 5 AND count <= 30")
 
-lucene_rs_list = list()
+tb_rs_list = list()
 
 for row in cursor:
-    lucene_rs_list.append(row[1])
+    tb_rs_list.append(row[1])
 
-random.shuffle(lucene_rs_list)
-final_list = lucene_rs_list[:50]
+random.shuffle(tb_rs_list)
+final_list = tb_rs_list[:25]
 
 for i in range(len(final_list)):
     print("PRINTING ISSUE NUMBER: ", i+1)
     current_title = final_list[i]
-    query = "SELECT title, comment, id, description, author, reporter, assignee, tagged FROM lucene_try WHERE title = %s"
+    query = "SELECT title, comment, id, description, author, reporter, assignee, tagged FROM thunderbird_try WHERE title = %s"
     cursor.execute(query, final_list[i])
     for row in cursor:
         root_comment = BeautifulSoup(row[1], "lxml").text
-        issue_id = row[2]
+        comment_id = row[2]
         author = row[4]
         reporter = row[5]
         assignee = row[6]
@@ -42,9 +42,9 @@ for i in range(len(final_list)):
         for j in range(len(cleancomment)):
             #print("Sub Comment: ", cleancomment[j])
             try:
-                cursor2.execute("""INSERT INTO lucene_rs (issue_id, title, root_comment, comment, author, reporter, assignee, tagged) VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s")""" % (issue_id, current_title, root_comment, cleancomment[j], author, reporter, assignee, tagged))
+                cursor2.execute("""INSERT INTO thunderbird_rs (comment_id, title, root_comment, comment, author, reporter, assignee, tagged) VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s")""" % (comment_id, current_title, root_comment, cleancomment[j], author, reporter, assignee, tagged))
             except:
-                print()
+                print("")
         print("\n")
 
 conn.close()
