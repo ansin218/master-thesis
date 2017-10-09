aa3260d33d50d48f20c49635525905b2a80aa056
Ankur Sinha <ankur218@yahoo.com>
2017-10-09 18:18:56 +0200

    Calculated Probability for stratified sampling

--- thesis-data-analysis/lucene-rss.py
+++ thesis-data-analysis/lucene-rss.py
@@ -11,17 +11,18 @@ import re
 start_time = time()
 
 conn = pymysql.connect(host='localhost', user='root', password='password', db='master_thesis', autocommit=True, use_unicode=True, charset="utf8")
-cursor_1 = conn.cursor()
-cursor_2 = conn.cursor()
-cursor_3 = conn.cursor()
-cursor_4 = conn.cursor()
-cursor_5 = conn.cursor()
-cursor2 = conn.cursor()
 
+insert_cursor = conn.cursor()
+
+cursor_1 = conn.cursor()
 cursor_1.execute("SELECT * FROM lucene_counter_rss WHERE count > 5 AND count <= 10")
+cursor_2 = conn.cursor()
 cursor_2.execute("SELECT * FROM lucene_counter_rss WHERE count > 10 AND count <= 15")
+cursor_3 = conn.cursor()
 cursor_3.execute("SELECT * FROM lucene_counter_rss WHERE count > 15 AND count <= 20")
+cursor_4 = conn.cursor()
 cursor_4.execute("SELECT * FROM lucene_counter_rss WHERE count > 20 AND count <= 25")
+cursor_5 = conn.cursor()
 cursor_5.execute("SELECT * FROM lucene_counter_rss WHERE count > 25 AND count <= 30")
 
 lucene_rs_list_1 = list()
@@ -45,16 +46,25 @@ for row in cursor_4:
 for row in cursor_5:
     lucene_rs_list_5.append(row[1])
 
-print(len(lucene_rs_list_1))
-print(len(lucene_rs_list_2))
-print(len(lucene_rs_list_3))
-print(len(lucene_rs_list_4))
-print(len(lucene_rs_list_5))
 total_len = len(lucene_rs_list_1) + len(lucene_rs_list_2) + len(lucene_rs_list_3) + len(lucene_rs_list_4) + len(lucene_rs_list_5)
 print("Total Issues: ", total_len)
 
-#random.shuffle(lucene_rs_list)
-#final_list = lucene_rs_list[:100]
+prob1 = round(len(lucene_rs_list_1)/total_len, 2) * 100
+prob2 = round(len(lucene_rs_list_2)/total_len, 2) * 100
+prob3 = round(len(lucene_rs_list_3)/total_len, 2) * 100
+prob4 = round(len(lucene_rs_list_4)/total_len, 2) * 100
+prob5 = round(len(lucene_rs_list_5)/total_len, 2) * 100
+
+random.shuffle(lucene_rs_list_1)
+final_list_1 = lucene_rs_list_1[:prob1]
+random.shuffle(lucene_rs_list_2)
+final_list_2 = lucene_rs_list_2[:prob2]
+random.shuffle(lucene_rs_list_3)
+final_list_3 = lucene_rs_list_3[:prob3]
+random.shuffle(lucene_rs_list_4)
+final_list_4 = lucene_rs_list_4[:prob4]
+random.shuffle(lucene_rs_list_5)
+final_list_5 = lucene_rs_list_5[:prob5]
 
 conn.close()
 
