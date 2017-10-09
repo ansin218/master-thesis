54c06ad08bde312a2f6fa528b4072a4a9221457e
Ankur Sinha <ankur218@yahoo.com>
2017-10-09 17:51:30 +0200

    Computed number of issues in each category for stratified sampling

--- thesis-data-analysis/lucene-rss.py
+++ thesis-data-analysis/lucene-rss.py
@@ -11,41 +11,50 @@ import re
 start_time = time()
 
 conn = pymysql.connect(host='localhost', user='root', password='password', db='master_thesis', autocommit=True, use_unicode=True, charset="utf8")
-cursor = conn.cursor()
+cursor_1 = conn.cursor()
+cursor_2 = conn.cursor()
+cursor_3 = conn.cursor()
+cursor_4 = conn.cursor()
+cursor_5 = conn.cursor()
 cursor2 = conn.cursor()
-cursor.execute("SELECT * FROM lucene_counter WHERE count > 5 AND count <= 30")
 
-lucene_rs_list = list()
+cursor_1.execute("SELECT * FROM lucene_counter_rss WHERE count > 5 AND count <= 10")
+cursor_2.execute("SELECT * FROM lucene_counter_rss WHERE count > 10 AND count <= 15")
+cursor_3.execute("SELECT * FROM lucene_counter_rss WHERE count > 15 AND count <= 20")
+cursor_4.execute("SELECT * FROM lucene_counter_rss WHERE count > 20 AND count <= 25")
+cursor_5.execute("SELECT * FROM lucene_counter_rss WHERE count > 25 AND count <= 30")
 
-for row in cursor:
-    lucene_rs_list.append(row[1])
+lucene_rs_list_1 = list()
+lucene_rs_list_2 = list()
+lucene_rs_list_3 = list()
+lucene_rs_list_4 = list()
+lucene_rs_list_5 = list()
 
-random.shuffle(lucene_rs_list)
-final_list = lucene_rs_list[:50]
+for row in cursor_1:
+    lucene_rs_list_1.append(row[1])
 
-for i in range(len(final_list)):
-    print("PRINTING ISSUE NUMBER: ", i+1)
-    current_title = final_list[i]
-    query = "SELECT title, comment, id, description, author, reporter, assignee, tagged FROM lucene_try WHERE title = %s AND id < 53960"
-    cursor.execute(query, final_list[i])
-    for row in cursor:
-        root_comment = BeautifulSoup(row[1], "lxml").text
-        issue_id = row[2]
-        author = row[4]
-        reporter = row[5]
-        assignee = row[6]
-        tagged = row[7]
-        print("Root Title: ", current_title)
-        #print("Description: ", description)
-        #print("Root Comment: ", root_comment)
-        cleancomment = tokenize.sent_tokenize(root_comment)
-        #for j in range(len(cleancomment)):
-            #print("Sub Comment: ", cleancomment[j])
-            #try:
-                #cursor2.execute("""INSERT INTO lucene_rs (issue_id, title, root_comment, comment, author, reporter, assignee, tagged) VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s")""" % (issue_id, current_title, root_comment, cleancomment[j], author, reporter, assignee, tagged))
-            #except:
-                #print()
-        #print("\n")
+for row in cursor_2:
+    lucene_rs_list_2.append(row[1])
+
+for row in cursor_3:
+    lucene_rs_list_3.append(row[1])
+
+for row in cursor_4:
+    lucene_rs_list_4.append(row[1])
+
+for row in cursor_5:
+    lucene_rs_list_5.append(row[1])
+
+print(len(lucene_rs_list_1))
+print(len(lucene_rs_list_2))
+print(len(lucene_rs_list_3))
+print(len(lucene_rs_list_4))
+print(len(lucene_rs_list_5))
+total_len = len(lucene_rs_list_1) + len(lucene_rs_list_2) + len(lucene_rs_list_3) + len(lucene_rs_list_4) + len(lucene_rs_list_5)
+print("Total Issues: ", total_len)
+
+#random.shuffle(lucene_rs_list)
+#final_list = lucene_rs_list[:100]
 
 conn.close()
 
