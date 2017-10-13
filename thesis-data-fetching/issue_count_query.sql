d2cb162afced5a3b9eb14bf8c698c3f9f79896ef
Ankur Sinha <ankur218@yahoo.com>
2017-10-13 08:58:32 +0200

    Sampling query for Ubuntu

--- thesis-data-fetching/issue_count_query.sql
+++ thesis-data-fetching/issue_count_query.sql
@@ -12,6 +12,9 @@ HAVING ( COUNT(title) >= 1 )
 ORDER BY count DESC;
 
 
+
+
+
 SELECT title AS title, count(*) AS count
 FROM thunderbird_try
 GROUP BY title
@@ -20,8 +23,17 @@ ORDER BY count DESC;
 
 
 
+
+
 SELECT title AS title, count(*) AS count
 FROM ubuntu_try
+GROUP BY title
+HAVING ( COUNT(title) >= 1 )
+ORDER BY count DESC;
+
+SELECT title AS title, count(*) AS count
+FROM ubuntu_try
+WHERE id >= 14567
 GROUP BY title
 HAVING ( COUNT(title) >= 1 )
 ORDER BY count DESC;
