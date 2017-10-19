62608cee68ecbde481f4f92a02f1f72c2b5020c0
Ankur Sinha <ankur218@yahoo.com>
2017-10-19 23:04:17 +0200

    Updated logic of Thunderbird RSS

--- thesis-data-analysis/thunderbird-rss.py
+++ thesis-data-analysis/thunderbird-rss.py
@@ -10,24 +10,29 @@ import re
 
 start_time = time()
 
-conn = pymysql.connect(host='localhost', user='root', password='password', db='master_thesis', autocommit=True, use_unicode=True, charset="utf8")
+conn = pymysql.connect(host='localhost', user='root', password='password', db='Issue_Trackers', autocommit=True, use_unicode=True, charset="utf8")
 
 insert_cursor_1 = conn.cursor()
 insert_cursor_2 = conn.cursor()
 insert_cursor_3 = conn.cursor()
 insert_cursor_4 = conn.cursor()
 insert_cursor_5 = conn.cursor()
+insert_cursor_6 = conn.cursor()
+insert_cursor_7 = conn.cursor()
+insert_cursor_8 = conn.cursor()
+insert_cursor_9 = conn.cursor()
+insert_cursor_10 = conn.cursor()
 
 cursor_1 = conn.cursor()
-cursor_1.execute("SELECT * FROM thunderbird_counter_rss WHERE count > 5 AND count <= 10")
+cursor_1.execute("SELECT * FROM thunderbird_issues_new WHERE comments_count > 5 AND comments_count <= 10")
 cursor_2 = conn.cursor()
-cursor_2.execute("SELECT * FROM thunderbird_counter_rss WHERE count > 10 AND count <= 15")
+cursor_2.execute("SELECT * FROM thunderbird_issues_new WHERE comments_count > 10 AND comments_count <= 15")
 cursor_3 = conn.cursor()
-cursor_3.execute("SELECT * FROM thunderbird_counter_rss WHERE count > 15 AND count <= 20")
+cursor_3.execute("SELECT * FROM thunderbird_issues_new WHERE comments_count > 15 AND comments_count <= 20")
 cursor_4 = conn.cursor()
-cursor_4.execute("SELECT * FROM thunderbird_counter_rss WHERE count > 20 AND count <= 25")
+cursor_4.execute("SELECT * FROM thunderbird_issues_new WHERE comments_count > 20 AND comments_count <= 25")
 cursor_5 = conn.cursor()
-cursor_5.execute("SELECT * FROM thunderbird_counter_rss WHERE count > 25 AND count <= 30")
+cursor_5.execute("SELECT * FROM thunderbird_issues_new WHERE comments_count > 25 AND comments_count <= 30")
 
 thunderbird_rs_list_1 = list()
 thunderbird_rs_list_2 = list()
@@ -65,23 +70,29 @@ print("Number of issues between 5 and 10 comments: ", prob1)
 for i in range(len(final_list_1)):
     print("PRINTING ISSUE NUMBER: ", i+1)
     current_title = final_list_1[i]
-    query = "SELECT title, comment, id, description, author, reporter, assignee, tagged FROM thunderbird_try WHERE id >= 28237 AND title = %s"
+    query = "SELECT * FROM thunderbird_issues_new, thunderbird_comments_new WHERE thunderbird_issues_new.issue_id = thunderbird_comments_new.issue_id AND title = %s"
     cursor_1.execute(query, final_list_1[i])
+    print(cursor_1.rowcount)
     for row in cursor_1:
-        root_comment = BeautifulSoup(row[1], "lxml").text
-        issue_id = row[2]
-        author = row[4]
-        reporter = row[5]
-        assignee = row[6]
-        tagged = row[7]
+        root_comment = BeautifulSoup(row[8], "lxml").text
+        issue_id = row[7]
+        comment_id = row[6]
+        author = row[9]
+        reporter = row[3]
+        assignee = row[4]
+        tagged = row[10]
         current_title = current_title.strip()
         print("Root Title: ", current_title)
         cleancomment = tokenize.sent_tokenize(root_comment)
+        try:
+            insert_cursor_1.execute("""INSERT INTO thunderbird_rss_issues (issue_id, title, reporter, assignee) VALUES ("%s", "%s", "%s", "%s")""" % (issue_id, current_title, reporter, assignee))
+        except:
+            print("")
         for j in range(len(cleancomment)):
             try:
-                insert_cursor_1.execute("""INSERT INTO thunderbird_rss (comment_id, title, root_comment, comment, author, reporter, assignee, tagged) VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s")""" % (issue_id, current_title, root_comment, cleancomment[j], author, reporter, assignee, tagged))
+                insert_cursor_6.execute("""INSERT INTO thunderbird_rss_comments (comment_id, issue_id, comment, sentence, author, tagged) VALUES ("%s", "%s", "%s", "%s", "%s", "%s")""" % (comment_id, issue_id, root_comment, cleancomment[j], author, tagged))
             except:
-                print()
+                print("")
         print("\n")
 
 random.shuffle(thunderbird_rs_list_2)
@@ -90,23 +101,27 @@ print("Number of issues between 10 and 15 comments: ", prob2)
 for i in range(len(final_list_2)):
     print("PRINTING ISSUE NUMBER: ", i+1)
     current_title = final_list_2[i]
-    query = "SELECT title, comment, id, description, author, reporter, assignee, tagged FROM thunderbird_try WHERE id >= 28237 AND title = %s"
+    query = "SELECT * FROM thunderbird_issues_new, thunderbird_comments_new WHERE thunderbird_issues_new.issue_id = thunderbird_comments_new.issue_id AND title = %s"
     cursor_2.execute(query, final_list_2[i])
     for row in cursor_2:
-        root_comment = BeautifulSoup(row[1], "lxml").text
-        issue_id = row[2]
-        author = row[4]
-        reporter = row[5]
-        assignee = row[6]
-        tagged = row[7]
-        current_title = current_title.strip()
+        root_comment = BeautifulSoup(row[8], "lxml").text
+        issue_id = row[7]
+        comment_id = row[6]
+        author = row[9]
+        reporter = row[3]
+        assignee = row[4]
+        tagged = row[10]
         print("Root Title: ", current_title)
         cleancomment = tokenize.sent_tokenize(root_comment)
+        try:
+            insert_cursor_2.execute("""INSERT INTO thunderbird_rss_issues (issue_id, title, reporter, assignee) VALUES ("%s", "%s", "%s", "%s")""" % (issue_id, current_title, reporter, assignee))
+        except:
+            print("")
         for j in range(len(cleancomment)):
             try:
-                insert_cursor_2.execute("""INSERT INTO thunderbird_rss (comment_id, title, root_comment, comment, author, reporter, assignee, tagged) VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s")""" % (issue_id, current_title, root_comment, cleancomment[j], author, reporter, assignee, tagged))
+                insert_cursor_7.execute("""INSERT INTO thunderbird_rss_comments (comment_id, issue_id, comment, sentence, author, tagged) VALUES ("%s", "%s", "%s", "%s", "%s", "%s")""" % (comment_id, issue_id, root_comment, cleancomment[j], author, tagged))
             except:
-                print()
+                print("")
         print("\n")
 
 random.shuffle(thunderbird_rs_list_3)
@@ -115,23 +130,28 @@ print("Number of issues between 15 and 20 comments: ", prob3)
 for i in range(len(final_list_3)):
     print("PRINTING ISSUE NUMBER: ", i+1)
     current_title = final_list_3[i]
-    query = "SELECT title, comment, id, description, author, reporter, assignee, tagged FROM thunderbird_try WHERE id >= 28237 AND title = %s"
+    query = "SELECT * FROM thunderbird_issues_new, thunderbird_comments_new WHERE thunderbird_issues_new.issue_id = thunderbird_comments_new.issue_id AND title = %s"
     cursor_3.execute(query, final_list_3[i])
     for row in cursor_3:
-        root_comment = BeautifulSoup(row[1], "lxml").text
-        issue_id = row[2]
-        author = row[4]
-        reporter = row[5]
-        assignee = row[6]
-        tagged = row[7]
+        root_comment = BeautifulSoup(row[8], "lxml").text
+        issue_id = row[7]
+        comment_id = row[6]
+        author = row[9]
+        reporter = row[3]
+        assignee = row[4]
+        tagged = row[10]
         current_title = current_title.strip()
         print("Root Title: ", current_title)
         cleancomment = tokenize.sent_tokenize(root_comment)
+        try:
+            insert_cursor_3.execute("""INSERT INTO thunderbird_rss_issues (issue_id, title, reporter, assignee) VALUES ("%s", "%s", "%s", "%s")""" % (issue_id, current_title, reporter, assignee))
+        except:
+            print("")
         for j in range(len(cleancomment)):
             try:
-                insert_cursor_3.execute("""INSERT INTO thunderbird_rss (comment_id, title, root_comment, comment, author, reporter, assignee, tagged) VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s")""" % (issue_id, current_title, root_comment, cleancomment[j], author, reporter, assignee, tagged))
+                insert_cursor_8.execute("""INSERT INTO thunderbird_rss_comments (comment_id, issue_id, comment, sentence, author, tagged) VALUES ("%s", "%s", "%s", "%s", "%s", "%s")""" % (comment_id, issue_id, root_comment, cleancomment[j], author, tagged))
             except:
-                print()
+                print("")
         print("\n")
 
 random.shuffle(thunderbird_rs_list_4)
@@ -140,23 +160,28 @@ print("Number of issues between 20 and 25 comments: ", prob4)
 for i in range(len(final_list_4)):
     print("PRINTING ISSUE NUMBER: ", i+1)
     current_title = final_list_4[i]
-    query = "SELECT title, comment, id, description, author, reporter, assignee, tagged FROM thunderbird_try WHERE id >= 28237 AND title = %s"
+    query = "SELECT * FROM thunderbird_issues_new, thunderbird_comments_new WHERE thunderbird_issues_new.issue_id = thunderbird_comments_new.issue_id AND title = %s"
     cursor_4.execute(query, final_list_4[i])
     for row in cursor_4:
-        root_comment = BeautifulSoup(row[1], "lxml").text
-        issue_id = row[2]
-        author = row[4]
-        reporter = row[5]
-        assignee = row[6]
-        tagged = row[7]
+        root_comment = BeautifulSoup(row[8], "lxml").text
+        issue_id = row[7]
+        comment_id = row[6]
+        author = row[9]
+        reporter = row[3]
+        assignee = row[4]
+        tagged = row[10]
         current_title = current_title.strip()
         print("Root Title: ", current_title)
         cleancomment = tokenize.sent_tokenize(root_comment)
+        try:
+            insert_cursor_4.execute("""INSERT INTO thunderbird_rss_issues (issue_id, title, reporter, assignee) VALUES ("%s", "%s", "%s", "%s")""" % (issue_id, current_title, reporter, assignee))
+        except:
+            print("")
         for j in range(len(cleancomment)):
             try:
-                insert_cursor_4.execute("""INSERT INTO thunderbird_rss (comment_id, title, root_comment, comment, author, reporter, assignee, tagged) VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s")""" % (issue_id, current_title, root_comment, cleancomment[j], author, reporter, assignee, tagged))
+                insert_cursor_9.execute("""INSERT INTO thunderbird_rss_comments (comment_id, issue_id, comment, sentence, author, tagged) VALUES ("%s", "%s", "%s", "%s", "%s", "%s")""" % (comment_id, issue_id, root_comment, cleancomment[j], author, tagged))
             except:
-                print()
+                print("")
         print("\n")
 
 random.shuffle(thunderbird_rs_list_5)
@@ -165,23 +190,28 @@ print("Number of issues between 25 and 30 comments: ", prob5)
 for i in range(len(final_list_5)):
     print("PRINTING ISSUE NUMBER: ", i+1)
     current_title = final_list_5[i]
-    query = "SELECT title, comment, id, description, author, reporter, assignee, tagged FROM thunderbird_try WHERE id >= 28237 AND title = %s"
+    query = "SELECT * FROM thunderbird_issues_new, thunderbird_comments_new WHERE thunderbird_issues_new.issue_id = thunderbird_comments_new.issue_id AND title = %s"
     cursor_5.execute(query, final_list_5[i])
     for row in cursor_5:
-        root_comment = BeautifulSoup(row[1], "lxml").text
-        issue_id = row[2]
-        author = row[4]
-        reporter = row[5]
-        assignee = row[6]
-        tagged = row[7]
+        root_comment = BeautifulSoup(row[8], "lxml").text
+        issue_id = row[7]
+        comment_id = row[6]
+        author = row[9]
+        reporter = row[3]
+        assignee = row[4]
+        tagged = row[10]
         current_title = current_title.strip()
         print("Root Title: ", current_title)
         cleancomment = tokenize.sent_tokenize(root_comment)
+        try:
+            insert_cursor_5.execute("""INSERT INTO thunderbird_rss_issues (issue_id, title, reporter, assignee) VALUES ("%s", "%s", "%s", "%s")""" % (issue_id, current_title, reporter, assignee))
+        except:
+            print("")
         for j in range(len(cleancomment)):
             try:
-                insert_cursor_5.execute("""INSERT INTO thunderbird_rss (comment_id, title, root_comment, comment, author, reporter, assignee, tagged) VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s")""" % (issue_id, current_title, root_comment, cleancomment[j], author, reporter, assignee, tagged))
+                insert_cursor_10.execute("""INSERT INTO thunderbird_rss_comments (comment_id, issue_id, comment, sentence, author, tagged) VALUES ("%s", "%s", "%s", "%s", "%s", "%s")""" % (comment_id, issue_id, root_comment, cleancomment[j], author, tagged))
             except:
-                print()
+                print("")
         print("\n")
 
 conn.close()
