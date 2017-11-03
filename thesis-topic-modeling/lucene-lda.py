e7cdfa6d71a15a21f6cd953f85d4c43a15048c00
Ankur Sinha <ankur218@yahoo.com>
2017-11-03 19:24:01 +0100

    Added basic topic modeling template using LDA

---
+++ thesis-topic-modeling/lucene-lda.py
@@ -0,0 +1,33 @@
+from nltk.tokenize import RegexpTokenizer
+from stop_words import get_stop_words
+from nltk.stem.porter import PorterStemmer
+from gensim import corpora, models
+import gensim
+
+tokenizer = RegexpTokenizer(r'\w+')
+en_stop = get_stop_words('en')
+p_stemmer = PorterStemmer()
+
+doc_a = "Brocolli is good to eat. My brother likes to eat good brocolli, but not my mother."
+doc_b = "My mother spends a lot of time driving my brother around to baseball practice."
+doc_c = "Some health experts suggest that driving may cause increased tension and blood pressure."
+doc_d = "I often feel pressure to perform well at school, but my mother never seems to drive my brother to do better."
+doc_e = "Health professionals say that brocolli is good for your health."
+
+doc_set = [doc_a, doc_b, doc_c, doc_d, doc_e]
+
+texts = []
+
+for i in doc_set:
+
+    raw = i.lower()
+    tokens = tokenizer.tokenize(raw)
+    stopped_tokens = [i for i in tokens if not i in en_stop]
+    stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
+    texts.append(stemmed_tokens)
+
+dictionary = corpora.Dictionary(texts)
+corpus = [dictionary.doc2bow(text) for text in texts]
+ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=2, id2word = dictionary, passes=20)
+
+print(ldamodel.print_topics(20))
