24cc204c0c6f4c11d98289040826fbf3b84765da
Ankur Sinha <ankur218@yahoo.com>
2017-11-10 20:21:43 +0100

    Converted labels and texts to numpy form

--- thesis-binary-classification/classifier-exp.py
+++ thesis-binary-classification/classifier-exp.py
@@ -1,7 +1,19 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import CountVectorizer
 
 data = pd.read_csv("lucene_Ankur.csv")
 
-print(type(data['sentence']))
+texts = []
+labels = []
+
+for i, label in enumerate(data['isRelevant']):
+    texts.append(data['sentence'][i])
+    if label == 0:
+        labels.append(0)
+    else:
+        labels.append(1)
+
+texts = np.asarray(texts)
+labels = np.asarray(labels)
