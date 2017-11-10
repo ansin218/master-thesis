802c90de4f98490ee8f8e6919862b04b4bcbc42e
Ankur Sinha <ankur218@yahoo.com>
2017-11-10 21:09:38 +0100

    Added basic Multinomial Naive Bayes Classifier to test on one example

--- thesis-binary-classification/classifier-exp.py
+++ thesis-binary-classification/classifier-exp.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
 
 data = pd.read_csv("lucene_Ankur.csv")
 
@@ -18,10 +19,16 @@ for i, label in enumerate(data['isRelevant']):
 texts = np.asarray(texts)
 labels = np.asarray(labels)
 
-print(np.unique(labels))
 print(np.bincount(labels))
 
 count_vectorizer = CountVectorizer()
 counts = count_vectorizer.fit_transform(texts)
 
-print(counts)
+classifier = MultinomialNB()
+targets = labels
+classifier.fit(counts, targets)
+
+examples = ["+1", "Nice"]
+example_counts = count_vectorizer.transform(examples)
+predictions = classifier.predict(example_counts)
+print(predictions)
