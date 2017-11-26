018b25369d2381a3126e4d4083c9da6c2fb9324c
Ankur Sinha <ankur218@yahoo.com>
2017-11-26 17:58:19 +0100

    Remove unwanted libraries and fix index

--- thesis-pos-tagging/lucene-pos.py
+++ thesis-pos-tagging/lucene-pos.py
@@ -1,15 +1,8 @@
 # -*- coding: utf-8 -*-
 import pymysql
 import pymysql.cursors
-from nltk.tokenize import RegexpTokenizer
-from stop_words import get_stop_words
-from nltk.stem.porter import PorterStemmer
-from gensim import corpora, models
-import gensim
 from time import time
 import spacy
-import pandas as pd
-import numpy as np
 import en_core_web_sm
 import json
 
@@ -26,7 +19,7 @@ for row in cursor_1:
     lucene_rss_list.append(row[4])
 
 for x in range(len(lucene_rss_list)):
-    print(lucene_rss_list[i])
+    print(lucene_rss_list[x])
 
 end_time = time()
 time_taken = end_time - start_time
