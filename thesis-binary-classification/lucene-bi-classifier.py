39f04c00d36faad95cb6ad40272f056619667f89
Ankur Sinha <ankur218@yahoo.com>
2017-12-02 22:14:11 +0100

    Tried to fix logistic regression result

--- thesis-binary-classification/lucene-bi-classifier.py
+++ thesis-binary-classification/lucene-bi-classifier.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 from time import time
 from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import KFold
 from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, precision_score, recall_score
@@ -15,6 +17,8 @@ data = pd.read_csv("a_lucene_results.csv")
 ######### LOGISTIC REGRESSION MODEL #########
 
 logit_pipeline = Pipeline([
+    #('vectorizer', CountVectorizer(ngram_range = (1, 10))),
+    #('tfidf_transformer', TfidfTransformer()),
     ('classifier', LogisticRegression())
 ])
 
@@ -26,25 +30,27 @@ logit_re_scores = []
 logit_conf_mat = np.array([[0, 0], [0, 0]])
 
 for train_indices, test_indices in k_fold.split(data):
+    try:
+        train_text = data.iloc[train_indices]['sentence'].values
+        train_y = data.iloc[train_indices]['isRelevant'].values
 
-    train_text = data.iloc[train_indices]['sentence'].values
-    train_y = data.iloc[train_indices]['isRelevant'].values
+        test_text = data.iloc[test_indices]['sentence'].values
+        test_y = data.iloc[test_indices]['isRelevant'].values
 
-    test_text = data.iloc[test_indices]['sentence'].values
-    test_y = data.iloc[test_indices]['isRelevant'].values
+        logit_pipeline.fit(train_text, train_y)
+        predictions = logit_pipeline.predict(test_text)
 
-    logit_pipeline.fit(train_text, train_y)
-    predictions = logit_pipeline.predict(test_text)
-
-    logit_conf_mat += confusion_matrix(test_y, predictions)
-    score1 = f1_score(test_y, predictions)
-    logit_f1_scores.append(score1)
-    score2 = accuracy_score(test_y, predictions)
-    logit_ac_scores.append(score2)
-    score3 = precision_score(test_y, predictions)
-    logit_pr_scores.append(score3)
-    score4 = recall_score(test_y, predictions)
-    logit_re_scores.append(score4)
+        logit_conf_mat += confusion_matrix(test_y, predictions)
+        score1 = f1_score(test_y, predictions)
+        logit_f1_scores.append(score1)
+        score2 = accuracy_score(test_y, predictions)
+        logit_ac_scores.append(score2)
+        score3 = precision_score(test_y, predictions)
+        logit_pr_scores.append(score3)
+        score4 = recall_score(test_y, predictions)
+        logit_re_scores.append(score4)
+    except:
+        print("Skipping Corrupt Lines")
 
 print("\nPrinting Results for Logistic Regression Model...")
 print("Comments Classified: ", len(data))
