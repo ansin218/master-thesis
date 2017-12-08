13df5d6906f7cf9e80a5dab46d95e08c379dc76a
Ankur Sinha <ankur218@yahoo.com>
2017-12-08 09:38:58 +0100

    Added lemmatizer instead of stemmer to LDA

--- thesis-topic-modeling/lucene-lda.py
+++ thesis-topic-modeling/lucene-lda.py
@@ -4,7 +4,8 @@ import pymysql.cursors
 from nltk.tokenize import RegexpTokenizer
 from stop_words import get_stop_words
 from nltk.stem.porter import PorterStemmer
-from gensim import corpora, models
+from nltk.stem.wordnet import WordNetLemmatizer
+from gensim import corpora, models, utils
 import gensim
 from time import time
 
@@ -22,7 +23,13 @@ for row in cursor_1:
 
 tokenizer = RegexpTokenizer(r'\w+')
 en_stop = get_stop_words('en')
+alphaList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+numList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+customList1 = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+en_stop = en_stop + alphaList + numList
+
 p_stemmer = PorterStemmer()
+lmtzr = WordNetLemmatizer()
 
 doc_set = lucene_rss_list
 
@@ -33,12 +40,12 @@ for i in doc_set:
     raw = i.lower()
     tokens = tokenizer.tokenize(raw)
     stopped_tokens = [i for i in tokens if not i in en_stop]
-    stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
+    stemmed_tokens = [lmtzr.lemmatize(i) for i in stopped_tokens]
     texts.append(stemmed_tokens)
 
 dictionary = corpora.Dictionary(texts)
 corpus = [dictionary.doc2bow(text) for text in texts]
-ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=2, id2word = dictionary, passes=20)
+ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=20, id2word = dictionary, passes=20)
 
 print(ldamodel.print_topics(20))
 
