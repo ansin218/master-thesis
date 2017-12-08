2a37b688d839f327aba5f5e366bc3169a235c7bd
Ankur Sinha <ankur218@yahoo.com>
2017-12-08 10:45:30 +0100

    Added 5 custom stop word lists

--- thesis-topic-modeling/lucene-lda.py
+++ thesis-topic-modeling/lucene-lda.py
@@ -25,8 +25,12 @@ tokenizer = RegexpTokenizer(r'\w+')
 en_stop = get_stop_words('en')
 alphaList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 numList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-customList1 = ['can', 'due', 'jira', 'lucene', 'instead', 'org', 'apache', 'hole']
-en_stop = en_stop + alphaList + numList
+customList1 = ['can', 'due', 'jira', 'lucene', 'instead', 'org', 'apache', 'hole', 'probably', 'use', 'another']
+customList2 = ['just', 'know', 'branch_3x', 'make', 'better', 'like', 'got', 'will', 've', 'see', 'afor', 'yes']
+customList3 = ['don', 'maybe', 'never', 'also', 'many', 'look', 'll', 'com', 'done', 'think', 'thank', 'might']
+customList4 = ['now', 'lon', 'method', 'much', 'need', 'sure', 'thanks', 'doesn', 'used', 'get', 'ok', '023']
+customList5 = ['well', 'since', 'using', 'rice', '64']
+en_stop = en_stop + alphaList + numList + customList1 + customList2 + customList3 + customList4 + customList5
 
 p_stemmer = PorterStemmer()
 lmtzr = WordNetLemmatizer()
@@ -45,9 +49,9 @@ for i in doc_set:
 
 dictionary = corpora.Dictionary(texts)
 corpus = [dictionary.doc2bow(text) for text in texts]
-ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=20, id2word = dictionary, passes=20)
+ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics = 10, id2word = dictionary, passes = 5)
 
-print(ldamodel.print_topics(20))
+print(ldamodel.print_topics(10))
 
 end_time = time()
 time_taken = end_time - start_time
