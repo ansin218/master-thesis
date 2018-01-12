aa02a615340e77288940126bf7a7cb63a9b39ffb
Ankur Sinha <ankur218@yahoo.com>
2018-01-12 13:36:05 +0100

    Updated Lucene Raw Data Comment Distribution

--- thesis-data-analysis/scripts/lucene_stats.R
+++ thesis-data-analysis/scripts/lucene_stats.R
@@ -1,14 +1,16 @@
 library(ggplot2)
 library(scales)
 
-luceneFile <- "lucene_count.csv"
+luceneFile <- "lucene_issues.csv"
 luceneData <- read.csv(luceneFile, header=TRUE, sep=",")
 
-luceneData$countCat <- cut(luceneData$COUNT, breaks=c(1,5,10,15,20,25,30,35,40,45,50,100,150))
+luceneData$countCat <- cut(luceneData$count, breaks=c(1,5,10,15,20,25,30,35,40,45,50,100,150))
+
+luceneData <- luceneData[!is.na(luceneData$countCat), ]
 
 ggplot(luceneData, aes(x = factor(luceneData$countCat))) +
   geom_bar(aes(y = (..count..))) +
   labs(x="Count Category",y="Count Frequency") +
-  ggtitle("Lucene Issue Tracker Comment Distribution") +
+  ggtitle("Apache Lucene Issue Trackers Comment Distribution") +
   theme(plot.title = element_text(hjust = 0.5)) +
   geom_text(aes( label = (..count..), y= ..count.. ), stat="count", vjust = -.5)
