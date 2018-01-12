2bcf57b5b0a9e4b68b1a6d763f42ee0e545e94b5
Ankur Sinha <ankur218@yahoo.com>
2018-01-12 13:42:08 +0100

    Updated Ubuntu Raw Data Comment Distribution

--- thesis-data-analysis/scripts/ubuntu_stats.R
+++ thesis-data-analysis/scripts/ubuntu_stats.R
@@ -1,14 +1,16 @@
 library(ggplot2)
 library(scales)
 
-ubuntuFile <- "ubuntu_count.csv"
+ubuntuFile <- "ubuntu_issues.csv"
 ubuntuData <- read.csv(ubuntuFile, header=TRUE, sep=",")
 
-ubuntuData$countCat <- cut(ubuntuData$COUNT, breaks=c(1,5,10,15,20,25,30,35,40,45,50,100,500))
+ubuntuData$countCat <- cut(ubuntuData$count, breaks=c(1,5,10,15,20,25,30,35,40,45,50,100,500))
+
+ubuntuData <- ubuntuData[!is.na(ubuntuData$countCat), ]
 
 ggplot(ubuntuData, aes(x = factor(ubuntuData$countCat))) +
   geom_bar(aes(y = (..count..))) +
   labs(x="Count Category",y="Count Frequency") +
-  ggtitle("Ubuntu Issue Tracker Comment Distribution") +
+  ggtitle("Ubuntu Issue Trackers Comment Distribution") +
   theme(plot.title = element_text(hjust = 0.5)) +
   geom_text(aes( label = (..count..), y= ..count.. ), stat="count", vjust = -.5)
