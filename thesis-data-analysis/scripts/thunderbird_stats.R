9504cbe588a2a061208428614f3bd50af3dec3a4
Ankur Sinha <ankur218@yahoo.com>
2018-01-12 13:40:41 +0100

    Updated Thunderbird Raw Data Comment Distribution

--- thesis-data-analysis/scripts/thunderbird_stats.R
+++ thesis-data-analysis/scripts/thunderbird_stats.R
@@ -1,14 +1,16 @@
 library(ggplot2)
 library(scales)
 
-thunderbirdFile <- "thunderbird_count.csv"
+thunderbirdFile <- "thunderbird_issues.csv"
 thunderbirdData <- read.csv(thunderbirdFile, header=TRUE, sep=",")
 
-thunderbirdData$countCat <- cut(thunderbirdData$COUNT, breaks=c(1,5,10,15,20,25,30,35,40,45,50,100,500))
+thunderbirdData$countCat <- cut(thunderbirdData$count, breaks=c(1,5,10,15,20,25,30,35,40,45,50,100,500))
+
+thunderbirdData <- thunderbirdData[!is.na(thunderbirdData$countCat), ]
 
 ggplot(thunderbirdData, aes(x = factor(thunderbirdData$countCat))) +
   geom_bar(aes(y = (..count..))) +
   labs(x="Count Category",y="Count Frequency") +
-  ggtitle("Thunderbird Issue Tracker Comment Distribution") +
+  ggtitle("Mozilla Thunderbird Issue Trackers Comment Distribution") +
   theme(plot.title = element_text(hjust = 0.5)) +
   geom_text(aes( label = (..count..), y= ..count.. ), stat="count", vjust = -.5)
