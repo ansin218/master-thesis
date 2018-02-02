266f1bf075c3827cca69d14ac8cda9e09bcfe310
Ankur Sinha <ankur218@yahoo.com>
2018-02-02 07:51:14 +0100

    Script update for Lucene LDA Keywords

--- thesis-linking-experiment/scripts/lucene-comments-lda.py
+++ thesis-linking-experiment/scripts/lucene-comments-lda.py
@@ -31,17 +31,35 @@ for x in range(len(lucene_rss_list)):
 
 	tokenizer = RegexpTokenizer(r'\w+')
 	en_stop = get_stop_words('en')
+	
 	alphaList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 	numList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-	customList1 = ['can', 'due', 'jira', 'lucene', 'instead', 'org', 'apache', 'hole', 'probably', 'use', 'another', 'looks', 'look', 'good']
-	customList2 = ['just', 'know', 'branch_3x', 'make', 'better', 'like', 'got', 'will', 've', 'see', 'afor', 'yes']
-	customList3 = ['don', 'maybe', 'never', 'also', 'many', 'look', 'll', 'com', 'done', 'think', 'thank', 'might', 'including', 'patch']
-	customList4 = ['now', 'lon', 'method', 'much', 'need', 'sure', 'thanks', 'doesn', 'used', 'get', 'ok', '023', 'issue', 'issues','git']
-	customList5 = ['well', 'since', 'using', 'rice', '64', 'havent', 'still', 'thats', '021', '128', 'hi', 'tell', 'say']
-	customList6 = ['example', 'thinking', 'data', 'dataset', 'move', 'branch', 'code', 'test', 'debug', 'debugging', 'ready']
-	customList7 = ['yeah', 'sorry', 'commit', 'push', 'merge', 'fix', 'issue', 'release', 'bulk', 'close', 'correctly', 'dynamically']
-	en_stop = en_stop + alphaList + numList + customList1 + customList2 + customList3 + customList4 + customList5 + customList6 + customList7
-
+	customList1 = ['can', 'due', 'jira', 'lucene', 'thunderbird', 'instead', 'org', 'apache', 'hole', 'probably', 'use', 'another', 'created']
+	customList2 = ['just', 'know', 'make', 'better', 'like', 'got', 'will', 've', 'see', 'afor', 'yes', 'comment', 'message', 'say']
+	customList3 = ['don', 'maybe', 'never', 'also', 'many', 'look', 'll', 'com', 'done', 'think', 'thank', 'might', 'inbox', 'address', 'already']
+	customList4 = ['now', 'lon', 'method', 'much', 'need', 'sure', 'thanks', 'doesn', 'used', 'get', 'ok', '023', 'reply', 'tb', 'either','try']
+	customList5 = ['well', 'since', 'using', 'rice', '64', '27', '21', '16', 'without', '14', '12', '19', '09', '25', '2013', 'mail', 'name']
+	customList6 = ['00', '1014', '10', '03', '28', 'still', '2016', '15', '11901', 'please', '30', '0050', '04', '026', 'may', 'email', 'yet', 'sorr']
+	customList7 = ['11', '31', '2147483647', '26', '13', 'ubuntu', 'common', 'find', 'found', 'version', 'something', 'seem', 'seems', 'comm', 'resolved']
+	customList8 = ['change', 'changes', 'changed', 'tool', 'tools', 'user', 'users', '32', '22', '17', 'status', 'linux', 'error', 'info', 'forget', 'forgot']
+	customList9 = ['patch', 'patches', 'issue', 'issues', 'bug', 'bugs', 'problem', 'problems', 'fix', 'fixes', 'fixed', 'test', 'tests', 'discussion', 'failure']
+	customList10 = ['management', 'usr', 'focus', 'application', 'log', 'package', 'packages', 'logs', 'work', 'worked', 'report', 'works', 'pick', 'picks']
+	customList11 = ['mozilla', 'messages', '06', 'www', 'http', 'tell', 'first', 'one', 'two', 'freiburg', 'uni', 'brain', 'window', 'wayne', 'soon', 'picked']
+	customList12 = ['true', 'false', 'wrong', 'way', '38', 'errors', 'correct', 'rev', 'tried', 'actually', 'want', 'wanted', 'disable', 'disabled', 'checked']
+	customList13 = ['merge', 'create', 'merged', 'created', 'good', 'err', 'maven', 'thing', 'trunk', 'commit', 'committed', 'pull', 'github', 'checks']
+	customList14 = ['adrien', 'robert', 'bulk', 'michael', 'git', 'svn', 'solr', 'merges', 'read', 'attached', 'current', 'required', 'moves', 'check']
+	customList15 = ['havent', 'store', 'stored', 'stores', 'one', 'close', 'look', 'looks', 'looked', 'revision', 'add', 'start', 'move', 'description']
+	customList16 = ['release', 'likewise', 'different', 'wait', 'confused', 'right', 'far', 'anyway', 'anyways', 'one', 'releases', 'branch', 'around']
+	customList17 = ['including', 'ready', 'fail', 'mechanism', 'incoming', 'dev', 'spell', 'thats', 'bit', 'bits', 'yeah', 'anywhere', 'suggests']
+	customList18 = ['push', 'pull', 'code', 'rule', 'feel', 'feels', 'felt', 'forgive', 'didnt', 'although', 'new', 'adding', 'reason', 'suggested']
+	customList19 = ['wanting', 'rule', 'legitimately', 'however', 'us', 'currently', 'way', 'ways', 'unnecessary', 'copy', 'pass', 'passed', 'suggest']
+	customList20 = ['come', 'testing', 'run', 'latest', 'round', 'rounds', 'update', 'updates', 'updated', 'agree', 'shall', 'arrive', 'arrived', 'line']
+	customList21 = ['tested', 'anythings', 'forms', 'help', 'easily', 'write', 'writes', 'lot', 'lots', 'aspect', 'comments', 'committing', 'thing']
+	customList22 = ['quick', 'keep', 'everything', 'non', 'correct', 'correctly', 'simple', 'clear', 'didnt', 'didn', 'time', 'times', 'likely', 'oh']
+	en_stop = en_stop + alphaList + numList + customList1 + customList2 + customList3 + customList4 + customList5 + customList14
+	en_stop = en_stop + customList6 + customList7 + customList8 + customList9 + customList10 + customList11 + customList12 + customList13
+	en_stop = en_stop + customList15 + customList16 + customList17 + customList18 + customList19 + customList20 + customList21 + customList22
+	
 	p_stemmer = PorterStemmer()
 	lmtzr = WordNetLemmatizer()
 
