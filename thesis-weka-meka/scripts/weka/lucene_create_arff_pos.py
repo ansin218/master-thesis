b4bcf2a9b1638530da67d320eb9d49437faaeece
Ankur Sinha <ankur218@yahoo.com>
2018-02-04 14:12:12 +0100

    Added ARFF script generator for WEKA

---
+++ thesis-weka-meka/scripts/weka/lucene_create_arff_pos.py
@@ -0,0 +1,38 @@
+_author_ = 'Rana'
+
+# generate arff file for working with weka
+
+import arff
+import pymysql
+
+
+# connect to the database
+
+db = pymysql.connect(host='localhost', user='root', password='password', db='Issue_Trackers')
+
+cursor = db.cursor()
+
+
+cursor.execute('SELECT sentence, isRelevant FROM a_lucene_pos_results')
+
+messages = cursor.fetchall()
+
+
+data = []
+
+
+# make data vector for each tagged msg
+for i, msg in enumerate(messages):
+    if msg[1]==1:
+        binary_class='relevant'
+    else:
+        binary_class = 'irrelevant'
+    data.append([msg[0], binary_class])
+
+
+arff.dump('Lucene_binary_pos.arff', data, relation="rationale", names=['sentence',  'classification'])
+
+
+cursor.close()
+db.close()
+
---
+++ thesis-weka-meka/scripts/weka/thunderbird_create_arff.py
@@ -0,0 +1,38 @@
+_author_ = 'Rana'
+
+# generate arff file for working with weka
+
+import arff
+import pymysql
+
+
+# connect to the database
+
+db = pymysql.connect(host='localhost', user='root', password='password', db='Issue_Trackers')
+
+cursor = db.cursor()
+
+
+cursor.execute('SELECT sentence, isRelevant FROM c_thunderbird_results')
+
+messages = cursor.fetchall()
+
+
+data = []
+
+
+# make data vector for each tagged msg
+for i, msg in enumerate(messages):
+    if msg[1]==1:
+        binary_class='relevant'
+    else:
+        binary_class = 'irrelevant'
+    data.append([msg[0], binary_class])
+
+
+arff.dump('Thunderbird_binary.arff', data, relation="rationale", names=['sentence',  'classification'])
+
+
+cursor.close()
+db.close()
+
